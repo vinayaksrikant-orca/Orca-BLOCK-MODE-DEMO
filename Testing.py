4d18f0f764669344c6ff732efb03d7a4b96c5ad0
vinayaksrikant-orca <vinayak.srikant@orca.security>
2026-09-23 17:51:51 +0530

    Delete Testing.py

--- Testing.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-from flask import request
-
-def get_user():
-    user_id = request.args.get("id")
-    conn = sqlite3.connect("test.db")
-    query = "SELECT * FROM users WHERE id = " + user_id
-    return conn.execute(query).fetchall()
